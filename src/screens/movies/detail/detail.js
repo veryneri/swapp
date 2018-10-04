2f699bc34f2585dbdada4ff4404b85f107b57621
Edson Neri <edson.neri@correo.uia.mx>
2018-10-04 13:26:40 -0500

    updates `MovieDetail` to show real info

--- src/screens/movies/detail/detail.js
+++ src/screens/movies/detail/detail.js
@@ -3,14 +3,45 @@ import {
   Text,
   View
 } from 'react-native';
+import { connect } from 'react-redux';
+import { getMovieDetail } from '../../../store/actions';
+import { ProgressiveImage } from '../../../components';
 
-export default class MovieList extends React.Component {
+class MovieDetail extends React.Component {
+  componentDidMount() {
+    const { imdbID } = this.props.navigation.state.params;
+    this.props.getMovieDetail(imdbID);
+  }
+
   render() {
-    return (
-      <View style={styles.view}>
-        <Text style={styles.text}>MovieLists works!</Text>
-      </View>
-    );
+    const {
+      loadingInfo,
+      movieInfo = {}
+    } = this.props;
+    const {
+      Poster,
+      Released,
+      Title,
+    } = movieInfo;
+    const content = loadingInfo
+      ? (
+        <View style={styles.view}>
+          <Text style={styles.text}>Loading...</Text>
+        </View>
+      )
+      : (
+        <View style={styles.view}>
+          <ProgressiveImage
+            source={{ uri: Poster}}
+            style={styles.image}
+            resizeMode='cover'
+          />
+          <Text style={styles.text}>{Title}</Text>
+          <Text style={styles.text}>{Released}</Text>
+        </View>
+      );
+
+    return content;
   }
 }
 
@@ -23,5 +54,24 @@ const styles = {
   text: {
     fontSize: 18,
     marginTop: 10
+  },
+  image: {
+    height: 466,
+    marginBottom: 15,
+    marginTop: 10,
+    width: 300
   }
 };
+
+const mapStateToProps = ({ loadingInfo, movieInfo }) => {
+  return {
+    loadingInfo,
+    movieInfo
+  };
+};
+
+const mapDispatchToProps = {
+  getMovieDetail
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieDetail);
