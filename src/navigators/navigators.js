cc31e1c86c85ba5943fa6d4efd0078bbb4a0b6e7
Edson Neri <edson.neri@correo.uia.mx>
2018-10-04 13:32:23 -0500

    adds `headerTitle` in every screen

--- src/navigators/navigators.js
+++ src/navigators/navigators.js
@@ -7,20 +7,32 @@ import MovieList from '../screens/movies/list/list';
 import Profile from '../screens/profile/profile';
 
 const Tabs = createBottomTabNavigator({
-  MovieList: {
-    screen: MovieList
+  'Movie List': {
+    screen: MovieList,
   },
   Profile: {
     screen: Profile
   }
 });
 
+Tabs.navigationOptions = ({ navigation }) => {
+  const { routeName } = navigation.state.routes[navigation.state.index];
+  const headerTitle = routeName;
+
+  return {
+    headerTitle,
+  };
+};
+
 const Stack = createStackNavigator({
   Home: {
     screen: Tabs
   },
   MovieDetail: {
-    screen: MovieDetail
+    screen: MovieDetail,
+    navigationOptions: {
+      title: 'Movie Detail'
+    }
   }
 });
 
