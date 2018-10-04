2da398df2da0ff5898267e1e9ad4a7c3e752cc11
Edson Neri <edson.neri@correo.uia.mx>
2018-10-04 01:23:51 -0500

    updates `App.js` to use recently created features
      - redux
      - navigators

--- App.js
+++ App.js
@@ -5,26 +5,26 @@
  * @format
  * @flow
  */
+import React from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from './src/store/configureStore';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import Stack from './src/navigators/navigators';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const store = configureStore();
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Stack />
+        </View>
+      </Provider>
     );
   }
 }
@@ -32,18 +32,7 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#fff'
+  }
 });
+
