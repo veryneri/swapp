8e43b030977d186d1a92aaa88cfd46922d1b0973
Edson Neri <edson.neri@correo.uia.mx>
2018-10-04 19:31:53 -0500

    updates `Profile` to display real information

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -1,14 +1,83 @@
 import React from 'react';
+import { View } from 'react-native';
 import {
-  Text,
-  View
-} from 'react-native';
+  InfoContainer,
+  ProgressiveImage
+} from '../../components';
+import Moment from 'moment';
+
+const USER = {
+  gender: 'female',
+  name: {
+    title: 'miss',
+    first: 'kristin',
+    last: 'armstrong'
+  },
+  location: {
+    street: 'ljan terrasse 346',
+    city: 'vear',
+    state: 'rogaland',
+    country: 'Hawaii',
+    postcode: '3095',
+    coordinates: {
+      latitude: '54.8646',
+      longitude: '-97.3136'
+    },
+    timezone: {
+      offset: '-10:00',
+      description: 'Hawaii'
+    }
+  },
+  email: 'kristin.armstrong@mail.com',
+  dob: {
+    date: '1989-11-12T06:34:44Z',
+    age: 28
+  },
+  picture: {
+    large: 'https://randomuser.me/api/portraits/women/67.jpg',
+    medium: 'https://randomuser.me/api/portraits/med/women/67.jpg',
+    thumbnail: 'https://randomuser.me/api/portraits/thumb/women/67.jpg'
+  }
+};
 
 export default class Profile extends React.Component {
   render() {
+    const {
+      dob,
+      location,
+      email,
+      name,
+      picture,
+    } = USER;
+    const displayDOB =  Moment(dob.date).format('L');
+
     return (
       <View style={styles.view}>
-        <Text style={styles.text}>Profile works!</Text>
+        <View style={styles.imageContainer}>
+          <ProgressiveImage
+            thumbnailSource={{ uri: picture.thumbnail }}
+            source={{ uri: picture.large}}
+            style={styles.image}
+            resizeMode='cover'
+        />
+        </View>
+
+        <InfoContainer
+          label='Name'
+          info={ `${name.first} ${name.last}` }
+        />
+        <InfoContainer
+          label='Email'
+          info={ email }
+        />
+        <InfoContainer
+          label='Birthday'
+          info={ displayDOB }
+        />
+        <InfoContainer
+          label='Location'
+          info={ location.country }
+        />
       </View>
     );
   }
@@ -20,8 +89,13 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'flex-start'
   },
-  text: {
-    fontSize: 18,
-    marginTop: 10
+  image: {
+    borderRadius: 67.5,
+    width: 125,
+    height: 125
+  },
+  imageContainer: {
+    marginTop: 50,
+    marginBottom: 50
   }
 };
